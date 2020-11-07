686cc7bd5feeb7602caafc75f08bd5d636d563b5
Vahid Akhtar <503191413>
2020-11-07 12:03:47 +0530

    initialDisplay component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import InitialDisplay from "./InitialDisplay";
 import "./styles.scss";
 
 export default function App() {
   return (
     <div className="App">
-      Stickynav routing
+      <InitialDisplay />
     </div>
   );
 }
---
+++ src/InitialDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InitialDisplay() {
+  return (
+    <div className="banner">
+     <img src="https://i.picsum.photos/id/299/5498/3615.jpg?hmac=NqSpUzSsD7mgU34riF8xDMZkcSQY345kONcjjOqqjBA" alt="banner"/>
+    </div>
+  )
+}
